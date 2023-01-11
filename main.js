ffa4e08b192743a2470a9a680582237458039885
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-01-11 11:28:30 -0300

    ok

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var iconMenu = document.querySelector('nav .menu-icon img');
 
 menuBar.addEventListener('click',function(){
 
-    if (iconMenu.getAttribute("src") == '../img/menu.png') {
-        iconMenu.setAttribute("src","../img/close.png");
+    if (iconMenu.getAttribute("src") == './img/menu.png') {
+        iconMenu.setAttribute("src","./img/close.png");
     }else{
-        iconMenu.setAttribute("src","../img/menu.png");
+        iconMenu.setAttribute("src","./img/menu.png");
     }
 
    menu.classList.toggle('active');
